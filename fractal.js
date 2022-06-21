5ada58a87d2eb1233eecbab87575f419e6954309
Shoupu Wan <swan@roblox.com>
2022-06-20 22:10:40 -0700

    finish the bulk part of fractal: koch circle

--- fractal.js
+++ fractal.js
@@ -1,14 +1,27 @@
 var nextTag = 0
-// init()
+var nextGen
+init()
 
 function init() {
   var svg = document.getElementById('svg');
-  width = svg.scrollWidth
-  height = svg.scrollHeight
-  newShape(width / 2, height / 2, (width + height) / 4, 0, 0)
+  size = svg.scrollWidth
+  var x = y = size / 2
+  var radius = size / 4/ Math.sqrt(2)
+  var element = newShape(size / 2, size / 2, radius, 0, 0)
+  svg.appendChild(element)
+
+  nextGen = [
+    { parent: [x, y], delta: [0, -radius] },
+    { parent: [x, y], delta: [0,  radius] },
+    { parent: [x, y], delta: [-radius, 0]},
+    { parent: [x, y], delta: [radius, 0] },
+  ];
 }
 
 function removeShapes() {
+  if (nextTag <= 0) {
+    return
+  }
   nextTag -= 1
   var tag = nextTag
   es = document.getElementsByClassName(tag)
@@ -22,13 +35,38 @@ function addShapes() {
   var tag = nextTag
   nextTag += 1
   var svg = document.getElementById('svg');
-  x = randint(svg.scrollWidth)
-  y = randint(svg.scrollHeight)
-  size = randint(100)
-  var element = newShape(x, y, size, tag, tag);
-  // cx="10" cy="10" r="5" stroke="black" stroke-width="1" fill="red" 
-  svg.appendChild(element);
-  console.log(svg);
+
+  var newGen = []
+  for (const spec of nextGen) {
+    let parent = spec["parent"]
+    let [dx,dy] = spec["delta"]
+    let radius = Math.abs(dx+dy) / 2
+    let x = parent[0] + dx * 3 / 2
+    let y = parent[1] + dy * 3 / 2
+    svg.appendChild(newShape(x, y, radius, tag, tag + '-' + x + '-' + y))
+
+    // calculate new peripheral centers (3 of them)
+    let cs = genPeripherals([x, y], [dx,dy])
+    for (const c of cs) {
+      newGen.push(c)
+    }
+    nextGen = newGen
+  }
+}
+
+function genDeltas(dx, dy) {
+  return [
+    [-dx, -dy],
+    [dy, dx],
+    [-dy, -dx],
+  ]
+}
+
+function genPeripherals(parent, delta) {
+  var [dx, dy] = delta
+  var cs = genDeltas(-dx / 2, -dy / 2)
+  var result = cs.map(c => { return { "parent": parent, "delta": c , "radius": Math.abs(dx+dy)/ 2 } })
+  return result
 }
 
 function randint(max) {
