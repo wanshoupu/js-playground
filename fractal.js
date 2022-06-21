b41b279ba85e3166b4048cccbea5f07a57f29059
Shoupu Wan <swan@roblox.com>
2022-06-20 22:41:26 -0700

    fix bug in removing multiple elements

--- fractal.js
+++ fractal.js
@@ -1,19 +1,19 @@
-var nextTag = 0
-var nextGen
+var nextTag = 1
+var nextGen = []
 init()
 
 function init() {
   var svg = document.getElementById('svg');
   size = svg.scrollWidth
   var x = y = size / 2
-  var radius = size / 4/ Math.sqrt(2)
+  var radius = size / 4 / Math.sqrt(2.5)
   var element = newShape(size / 2, size / 2, radius, 0, 0)
   svg.appendChild(element)
 
   nextGen = [
     { parent: [x, y], delta: [0, -radius] },
-    { parent: [x, y], delta: [0,  radius] },
-    { parent: [x, y], delta: [-radius, 0]},
+    { parent: [x, y], delta: [0, radius] },
+    { parent: [x, y], delta: [-radius, 0] },
     { parent: [x, y], delta: [radius, 0] },
   ];
 }
@@ -24,11 +24,11 @@ function removeShapes() {
   }
   nextTag -= 1
   var tag = nextTag
-  es = document.getElementsByClassName(tag)
+  es = [...document.getElementsByClassName(tag)]
   var svg = document.getElementById('svg');
-  for (let e of es) {
-    svg.removeChild(e)
-  }
+  es.forEach(element => {
+    svg.removeChild(element)    
+  });
 }
 
 function addShapes() {
@@ -39,14 +39,15 @@ function addShapes() {
   var newGen = []
   for (const spec of nextGen) {
     let parent = spec["parent"]
-    let [dx,dy] = spec["delta"]
-    let radius = Math.abs(dx+dy) / 2
+    let [dx, dy] = spec["delta"]
+    let radius = Math.abs(dx + dy) / 2
     let x = parent[0] + dx * 3 / 2
     let y = parent[1] + dy * 3 / 2
-    svg.appendChild(newShape(x, y, radius, tag, tag + '-' + x + '-' + y))
+    let id = tag + '-' + x + '-' + y + '-' + radius
+    svg.appendChild(newShape(x, y, radius, tag, id))
 
     // calculate new peripheral centers (3 of them)
-    let cs = genPeripherals([x, y], [dx,dy])
+    let cs = genPeripherals([x, y], [dx, dy])
     for (const c of cs) {
       newGen.push(c)
     }
@@ -65,13 +66,10 @@ function genDeltas(dx, dy) {
 function genPeripherals(parent, delta) {
   var [dx, dy] = delta
   var cs = genDeltas(-dx / 2, -dy / 2)
-  var result = cs.map(c => { return { "parent": parent, "delta": c , "radius": Math.abs(dx+dy)/ 2 } })
+  var result = cs.map(c => { return { "parent": parent, "delta": c, "radius": Math.abs(dx + dy) / 2 } })
   return result
 }
 
-function randint(max) {
-  return Math.floor(Math.random() * max);
-}
 
 function newShape(x, y, size, tag, id) {
   var element = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
